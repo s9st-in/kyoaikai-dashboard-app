57906be8dbabbf2fe0838bb934a507b4e4282437
Takashi Osako <skxt.dev@gmail.com>
2026-05-08 13:38:41 +0900

    initial commit

---
+++ _app/immutable/chunks/C6sTHrjy.js
@@ -0,0 +1,1 @@
+import{s as t,p as e}from"./BXLJoGTZ.js";const a={get data(){return e.data},get url(){return e.url}};t.updated.check;const p=a;export{p};
--- _app/immutable/entry/start.BCI2Oh9t.js
+++
@@ -1,1 +0,0 @@
-import{l as o,c as r}from"../chunks/6HMI67aj.js";export{o as load_css,r as start};
